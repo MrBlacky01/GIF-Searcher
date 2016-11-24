c9368e59eeef6e837396d9fc36455ad4fc868bf1
Dmitry <mr.blacky01@gmail.com>
2016-11-24 03:53:11 +0300

    +Add icon for gif, layout of gif and make structure of main activity layout

--- GIF-Searcher/app/src/main/java/com/blacky/mr/gifsearcher/Activities/MainActivity.java
+++ GIF-Searcher/app/src/main/java/com/blacky/mr/gifsearcher/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.blacky.mr.gifsearcher;
+package com.blacky.mr.gifsearcher.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blacky.mr.gifsearcher.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ GIF-Searcher/app/src/main/java/com/blacky/mr/gifsearcher/Models/GifState.java
@@ -0,0 +1,6 @@
+package com.blacky.mr.gifsearcher.Models;
+
+public class GifState {
+
+
+}
